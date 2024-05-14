1e2c0d0271abb34b3341c360fc5defe5b55e5f9d
Mrzunnnn <148939431+Mrzunnnn@users.noreply.github.com>
2024-05-14 22:34:22 +0700

    .

--- demo/src/main/java/vn/techmaster/demo/controller/ProductController.java
+++ demo/src/main/java/vn/techmaster/demo/controller/ProductController.java
@@ -3,6 +3,7 @@ package vn.techmaster.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import org.springframework.ui.Model;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-@RestController
+@Controller
 @RequestMapping("/products")
 public class ProductController {
     @Autowired
