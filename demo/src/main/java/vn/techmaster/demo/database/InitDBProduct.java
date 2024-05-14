416f8fcaa4ed8e178e208178acaa307435450cd1
Mrzunnnn <148939431+Mrzunnnn@users.noreply.github.com>
2024-05-14 20:10:51 +0700

    up

--- demo/src/main/java/vn/techmaster/demo/database/InitDBProduct.java
+++ demo/src/main/java/vn/techmaster/demo/database/InitDBProduct.java
@@ -3,8 +3,7 @@ package vn.techmaster.demo.database;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
-import vn.techmaster.demo.model.Product;
-import vn.techmaster.demo.utils.file.IFileReader;
+
 import vn.techmaster.demo.utils.file.IFileReaderProduct;
 @Configuration
 public class InitDBProduct implements CommandLineRunner {
